6540abd9ee0ef16bcb17b5288fc40970e6fedf91
Robert Schwartz <bschwartz23@gmail.com>
2021-07-13 16:42:45 -0700

    install jquery and bootstrap npm packages.  removed scripts for jquery and bootstrap in all html files.  Removed vender file containing previous Jquery and Bootstrap files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
-// require path node module
+// require packages
 const path = require("path");
+const webpack = require("webpack");
 
 // main configuration options object
+//============================================================
+// Must contain entry, output, mode.  the rest are optional
+// entry point is the root of the bundle/beginning of the dependency graph
+// output the bundled code to the folder we specify
+// mode in which we want webpack to run (development or production)
+// plugins - directions on how to opporate
 module.exports = {
-    // entry point is the root of the bundle/beginning of the dependency graph
-    // output the bundled code to the folder we specify
-    // mode in which we want webpack to run
     entry: './assets/js/script.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
     mode: 'development'
 };
