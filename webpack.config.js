4e18ff5a4c7e1c948b4f4f168123fbc3ecd5ce56
Robert Schwartz <bschwartz23@gmail.com>
2021-07-14 15:11:57 -0700

    Code Split the App to Separate Concerns

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 
 // main configuration options object
@@ -11,20 +13,65 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 // output the bundled code to the folder we specify
 // mode in which we want webpack to run (development or production)
 // plugins - directions on how to operate
-module.exports = {
-    entry: './assets/js/script.js',
+const config = {
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return '[path][name].[ext]';
+                            },
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery'
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+            analyzerMode: 'static'
         })
+        // new WebpackPwaManifest({
+        //   name: "Food Event",
+        //   short_name: "Foodies",
+        //   description: "An app that allows you to view upcoming food events.",
+        //   background_color: "#01579b",
+        //   theme_color: "#ffffff",
+        //   fingerprints: false,
+        //   inject: false,
+        //   icons: [{
+        //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+        //     sizes: [96, 128, 192, 256, 384, 512],
+        //     destination: path.join("assets", "icons")
+        //   }]
+        // })
     ],
     mode: 'development'
 };
+
+module.exports = config;
