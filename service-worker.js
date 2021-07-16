d76a569304c87a180346c4cc2f659081836cb2ac
Robert Schwartz <bschwartz23@gmail.com>
2021-07-16 15:41:27 -0700

    intercept Fetch Requests

--- service-worker.js
+++ service-worker.js
@@ -55,3 +55,25 @@ self.addEventListener('activate', function (e) {
     );
 });
 
+// Intercept Fetch Requests
+// ==========================================
+// listen for the fetch event
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        // determine if the resource already exists in caches, then log URL 
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
+
