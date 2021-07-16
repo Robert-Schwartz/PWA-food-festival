d74df86041263656b8ddbf7d4a4c3f6006fb7b7f
Robert Schwartz <bschwartz23@gmail.com>
2021-07-15 17:48:46 -0700

    installed npm packages -D file-loader to process image files and  image-webpack-loader to reduce image sizes

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(jpg|png|jpe?g|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
@@ -56,20 +56,6 @@ const config = {
         new BundleAnalyzerPlugin({
             analyzerMode: 'static'
         })
-        // new WebpackPwaManifest({
-        //   name: "Food Event",
-        //   short_name: "Foodies",
-        //   description: "An app that allows you to view upcoming food events.",
-        //   background_color: "#01579b",
-        //   theme_color: "#ffffff",
-        //   fingerprints: false,
-        //   inject: false,
-        //   icons: [{
-        //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-        //     sizes: [96, 128, 192, 256, 384, 512],
-        //     destination: path.join("assets", "icons")
-        //   }]
-        // })
     ],
     mode: 'development'
 };
