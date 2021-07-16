7280937099b0313d4ab9e12f22ab16bd0ac41f8e
Robert Schwartz <bschwartz23@gmail.com>
2021-07-15 17:35:54 -0700

    udate script tags in each html sheet to point to new bundle file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 require('bootstrap');
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+const img1 = require("../img/food-table.jpg");
+const img2 = require("../img/grill.jpg");
 
 $(document).ready(function() {
   // First image is hard coded in index.html
@@ -20,7 +20,7 @@ $(document).ready(function() {
       btnUrl: "tickets.html"
     },
   ];
-  
+
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
@@ -29,10 +29,10 @@ $(document).ready(function() {
           <div class="row align-items-center justify-content-center">
               <h2 class="display-4 mb-2">${slide.title}</h2>
           </div>
-          <div class="row align-items-center justify-content-center"> 
+          <div class="row align-items-center justify-content-center">
             <h3>${slide.subtitle}</h3>
           </div>
-          <div class=" mt-4 row align-items-center justify-content-center"> 
+          <div class=" mt-4 row align-items-center justify-content-center">
             <a class="btn btn-primary" href="${slide.btnUrl}">
                 ${slide.btnText}
             </a>
